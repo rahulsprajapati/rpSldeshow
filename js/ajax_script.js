e68f2499d97bf641609a5c0a13b9a04322c6146d
rahul.prajapati@live.in <rahul.prajapati@live.in>
2015-01-01 23:32:27 +0530

    update all

--- js/ajax_script.js
+++ js/ajax_script.js
@@ -68,31 +68,37 @@ jQuery(document).ready(function($) {
 
 	// Add Images 
 	$("#form1").submit(function(event) {
-		$(".rp_loader").show();
-		$.ajax({
-			url : ajaxurl, 
-			type : "POST", 
-			data : new FormData(this),
-			contentType : false,
-			cache : false,
-			processData : false, 
-			success : function(data) 
-			{
-				$("#form1")[0].reset();
-				$(".rp_loader").hide();
-				loadImageList();
-				if(data == "0"){
-					$(".rp_msgBox").html("Slide Added Successfully...!!!");
-				}else if(data == "1"){
-					$(".rp_msgBox").html("There is some problem in uploading. Please Try again.");
-				}else if(data == "2"){
-					$(".rp_msgBox").html("This image Cannot Upload. (minimum width=640 and minimum height=340 )");
+		if($("#slide1").val() != ""){
+			$(".rp_loader").show();
+			$.ajax({
+				url : ajaxurl, 
+				type : "POST", 
+				data : new FormData(this),
+				contentType : false,
+				cache : false,
+				processData : false, 
+				success : function(data) 
+				{
+					$("#form1")[0].reset();
+					$(".rp_loader").hide();
+					loadImageList();
+					if(data == "0"){
+						$(".rp_msgBox").html("Slide Added Successfully...!!!");
+					}else if(data == "1"){
+						$(".rp_msgBox").html("There is some problem in uploading. Please Try again.");
+					}else if(data == "2"){
+						$(".rp_msgBox").html("This image Cannot Upload. (minimum width=640 and minimum height=340 )");
+					}
+					$(".rp_msgBox").fadeIn(1000).delay(2000).fadeOut();				
+					$("#imageSrc").html("No Image Is Selected");
+					$("#form1").reset();
 				}
-				$(".rp_msgBox").fadeIn(1000).delay(2000).fadeOut();				
-				$("#imageSrc").html("No Image Is Selected");
-				$("#form1").reset();
-			}
-		});
+			});
+		}else{
+			$(".rp_msgBox").html("Slide cannot be added.Please Upload Image First.");
+			$(".rp_msgBox").fadeIn(1000).delay(2000).fadeOut();				
+			
+		}
 		return false;
 	});
 	loadImageList();
